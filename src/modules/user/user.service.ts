ba26922c1cbcaa5e93d8fea6551cd5dccf64e584
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-19 11:37:39 +0600

    feat:update user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,13 @@
 import { User } from "../../generated/prisma/client"
 import { prisma } from "../../lib/prisma"
-
+import bcrypt from "bcrypt";
 const registerUser = async (data: User) => {
+    const hashPassword = await bcrypt.hash(data.password!, 10)
     return await prisma.user.create({
-        data
+        data: {
+            ...data,
+            password: hashPassword
+        }
     })
 }
 export const userServices = { registerUser }
