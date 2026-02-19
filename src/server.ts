115c506ddf6cc48c864634bc6abcd56255d14781
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-19 09:06:17 +0600

    updat

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+const PORT = process.env.PORT || 4000
+function server() {
+    app.listen(PORT, () => {
+        console.log(`server is running on ${PORT}`)
+    })
+
+}
+server()
